e12f80da934b96dff43136a41050991bbe953591
php-may <75525895+php-may@users.noreply.github.com>
2020-12-28 11:56:11 +0200

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	require "index.html";
-	$message = 'Это тестовое сообщение в мире!';
-	$to = "davittchemi555@gmail.com";
-	$subject = 'Тестовая тема';
-	mail ($to, $subject, $message);
- ?>
